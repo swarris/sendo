105a887bfdfb8ce2b6c660bded6d19e3ef4af9c0
Sven Warris <sven@warris.nl>
2017-12-21 11:12:16 +0100

    Added script to prepare interproscan run on Slurm cluster

---
+++ interproscan/splitFile.py
@@ -0,0 +1,31 @@
+import sys
+from Bio import SeqIO
+import os
+
+fileName = sys.argv[1]
+
+splitAt = 100
+index = 0
+count = 0
+
+if not os.path.exists("input/{}/{}".format(fileName,index)):
+    os.makedirs("input/{}/{}".format(fileName,index))
+
+if not os.path.exists("output/{}/{}".format(fileName,index)):
+    os.makedirs("output/{}/{}".format(fileName,index))
+
+outFile = open("input/{}/{}/{}.{}".format(fileName,index, fileName, index), "w")
+for i in SeqIO.parse(open(fileName, "r"),"fasta"):
+    if count == splitAt:
+        count = 0
+        index += 1
+        outFile.close()
+        if not os.path.exists("input/{}/{}".format(fileName,index)):
+            os.makedirs("input/{}/{}".format(fileName,index))
+        if not os.path.exists("output/{}/{}".format(fileName,index)):
+            os.makedirs("output/{}/{}".format(fileName,index))
+        outFile = open("input/{}/{}/{}.{}".format(fileName,index, fileName, index), "w")
+    count += 1
+    SeqIO.write(i,outFile,"fasta")
+
+print("sbatch -a 0-{} run_interproscan.sh {}".format(index, fileName))

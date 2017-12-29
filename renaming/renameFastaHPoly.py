6d2b2927d5a63678401ae2adf84f5cdfe54efec2
Sven Warris <sven@warris.nl>
2017-12-29 14:14:06 +0100

    Added __[0-9]{4}__ to name sso order is kept when sorting

--- renaming/renameFastaHPoly.py
+++ renaming/renameFastaHPoly.py
@@ -1,11 +1,12 @@
 import sys
 
 result = open(sys.argv[2], "w")
-
+index = 1
 for l in open(sys.argv[1], "r"):
     if l[0] == ">":
-        label = l[1:].split("_")[2].strip()
+        label = "__{:04}__".format(index) + l[1:].split("_")[2].strip()
         result.write(">{}\n".format(label))
         print("{}\t{}".format(l[1:].strip(), label))
+        index += 1
     else:
         result.write(l)

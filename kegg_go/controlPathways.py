bfbb7dba5819da83858bb5563ea89d25be27e66a
Sven Warris <sven@warris.nl>
2018-01-03 16:54:19 +0100

    Script to check pathways of model orgs.

--- kegg_go/controlPathways.py
+++ kegg_go/controlPathways.py
@@ -6,26 +6,31 @@ from Bio.Graphics.ColorSpiral import ColorSpiral
 from collections import defaultdict
 import sys
 import os
+import re
 
 from pprint import pprint
 
+
 """
-python3 drawPathways.py kegg.txt *.faa
+python3 drawPathways.py keggmap kegg.txt control.faa
+keggmap: KEGG map identifier
 kegg.txt: gene ids (<xref id=) + kegg ids
-*.faa: protein files for IDs
+control.faa: protein file for IDs
 """
 
-colorCodes = {"inAll":"#25DE01",
-              "notInSendo":"#F8B10D",
-              "onlyInSendo":"#0D3CFD"}
+colorCodes = {"in":"#39818e",
+              "out":"#c81837",
+              "notRefIn":"#fcd549",
+              "notRefOut": "#f77524"}
 
-mb42Name = "MB42_proteins"
-levName = "LEV6574_proteins"
+pattern = re.compile("\[EC:(.*?)\]")
 
+keggmap = sys.argv[1]
 kegg = defaultdict(list)
 
 ecToGene = defaultdict(list)
 ecToGeneOrg = {}
+ecToRefGene = defaultdict(list)
 KOToEC = defaultdict(list)
 KOToGene = defaultdict(list)
 
@@ -37,7 +42,7 @@ currentGene = ""
 # expand org list with files and extract IDs:
 
 proteinMapping = {}
-for f in sys.argv[2:]:
+for f in sys.argv[3:]:
     orgName = f.split("/")[-1].split(".")[0]
     print(orgName)
     orgList.append(orgName)
@@ -45,7 +50,7 @@ for f in sys.argv[2:]:
         if len(l) > 0 and l[0] == ">":
             proteinMapping[l[1:].split(" ")[0].strip()] = orgName
     
-for l in open(sys.argv[1],"r"):
+for l in open(sys.argv[2],"r"):
     if "<xref id=" in l:
         currentGene = l.split('"')[1]
     else:
@@ -59,14 +64,40 @@ for l in open(sys.argv[1],"r"):
                         ecToGeneOrg[org] = defaultdict(list)
                     if currentGene not in ecToGeneOrg[org][ec]:
                         ecToGeneOrg[org][ec].append(currentGene)
-
 # process all found kegg pathways
 for k in kegg:
     print("Processing: {}".format(k))
     stats[k] = defaultdict(int)
     # load current pathway
-    pathway = KGML_parser.read(kegg_get("ko{}".format(k), "kgml"))
+    isRef = True
+    try:
+        pathway = KGML_parser.read(kegg_get("{}{}".format(keggmap,k), "kgml"))
+    except:
+        print("{} not a reference pathway.".format(k))
+        pathway = KGML_parser.read(kegg_get("ko{}".format(k), "kgml"))
+        isRef = False
+        
+        
     canvas = KGMLCanvas(pathway, import_imagemap=True)
+    if isRef:
+        for element in list(pathway.genes):
+            f = kegg_find(keggmap, element.name)
+            results = f.readline() 
+            g = results.split("\t")[0]#[len(keggmap)+1:]
+            
+            for l in kegg_get(g).readlines():
+                if "ORTHOLOGY" in l:
+                    match = pattern.findall(l) # EC number
+                    ortho= l[12:18] 
+                    if not match == None and len(match) > 0:
+                        KOToEC[element.name].extend(match[0].split(" "))
+                        KOToEC[ortho].extend(match[0].split(" "))
+                        
+                        #print("Added {} to {}".format(match[0].split(" "),element.name.split(":")[1]))
+                        #print(results)
+                        #print(l)
+                    
+                
     
     # get information on EC numbers in kegg pathway
     for ec in kegg[k]:
@@ -76,6 +107,9 @@ for k in kegg:
             # query KEGG
             for ecInfo in kegg_get("ec:{}".format(ec)):
                 ecInfoLabel = ecInfo[:12]
+                #if len(ecInfoLabel.strip()) > 0: 
+                #    print(ecInfo.strip())
+                      
                 if "ORTHOLOGY" in ecInfoLabel:
                     foundOrtho = True
                     KOToEC[ecInfo[12:18]].append(ec)
@@ -85,49 +119,50 @@ for k in kegg:
                     if foundOrtho:
 #                        KOToGene[ecInfo[12:18]].extend(ecToGene[ec])
                         KOToEC[ecInfo[12:18]].append(ec)
-    # check all species:
-    if not os.path.exists("paths/{}_{}_ALL.pdf".format(k, pathway.title.replace("/","_"))):
-        for element in pathway.orthologs:
+    #print(KOToEC)
+    if not os.path.exists("paths/{}_{}_{}.pdf".format(k, pathway.title.replace("/","_"), keggmap)):
+        if isRef:
+            elements = list(pathway.genes) + list(pathway.orthologs)
+        else:
+            elements = list(pathway.orthologs)
+        for element in elements:
+            #print(element.name)
+        #for element in pathway.genes:
             for graphic in element.graphics:
                 #graphic.name = "myEC"
+                #print("  " + graphic.name)
                 stats[k]["all"] += 1 
-                if graphic.name[:6] in KOToEC:
+                if graphic.name[:6] in KOToEC or element.name in KOToEC:
+                    if graphic.name[:6] in KOToEC:
+                        name = graphic.name[:6]
+                    else:
+                        name = element.name
+                    #print(name)
+                        
                     if graphic.bgcolor not in colorCodes.values():
                         defaultBG = graphic.bgcolor
-                    mb42 = [ec for ec in KOToEC[graphic.name[:6]] if ec in ecToGeneOrg[mb42Name]]
-                    lev = [ec for ec in KOToEC[graphic.name[:6]] if ec in ecToGeneOrg[levName]]
                     orgOverview = defaultdict(list)
                     orgCount = 0
                     for o in ecToGeneOrg.keys():
-                        orgOverview[o] = [ec for ec in KOToEC[graphic.name[:6]] if ec in ecToGeneOrg[o]]
+                        orgOverview[o] = [ec for ec in KOToEC[name] if ec in ecToGeneOrg[o]]
                         if len(orgOverview[o]) > 0:
                             stats[k][o] += 1
-                            if o not in [mb42Name,levName]:
-                                orgCount += 1
-
-                    #colorCodes = {"inAll":"#25DE01", "notInSendo":"#F8B10D", "onlyInSendo":"#0D3CFD"}
-
-                    if len(mb42) > 0 or len(lev) > 0: # sendo found
-                        if len(mb42) > 0 and len(lev) > 0:
-                            graphic.name = "S {}; C {}".format("2", orgCount)
-                        elif len(mb42) > 0:
-                            graphic.name = "S {}; C {}".format("1", orgCount)
-                        else:
-                            graphic.name = "S {}; C {}".format("1", orgCount)
-                        if orgCount > 0:
-                            graphic.bgcolor = colorCodes["inAll"]
-                        else:
-                            graphic.bgcolor = colorCodes["onlyInSendo"]
-                            
-                    elif orgCount > 0: # no sendo
-                        graphic.name = "S {}; C {}".format(0, orgCount) 
-                        graphic.bgcolor = colorCodes["notInSendo"]
+                            orgCount += 1
+                    if orgCount > 0 and isRef:
+                        graphic.bgcolor = colorCodes["in"]
+                    elif isRef and orgCount == 0:
+                        graphic.bgcolor = colorCodes["out"]
+                    elif orgCount > 0 and not isRef:
+                        graphic.bgcolor = colorCodes["notRefIn"]
+                    elif orgCount == 0 and not isRef:                        
+                        graphic.bgcolor = colorCodes["notRefOut"]
                         
-        canvas.draw("paths/{}_{}_ALL.pdf".format(k, pathway.title.replace("/","_")))
+        canvas.draw("paths/{}_{}_{}.pdf".format(k, pathway.title.replace("/","_"), keggmap))
     else:
-        print("paths/{}_{}_ALL.pdf exists. Skipping.".format(k, pathway.title.replace("/","_")))
+        print("paths/{}_{}_{}.pdf exists. Skipping.".format(k, pathway.title.replace("/","_"), keggmap))
+    
 
-statsF = open("stats.csv", "w")
+statsF = open("stats_{}.csv".format(keggmap), "w")
 statsF.write("KEGG\tTotal\t" + "\t".join(ecToGeneOrg.keys()) + "\n")
 for k in stats.keys():
     statsF.write("{}\t{}\t".format(k, stats[k]["all"]))
@@ -141,28 +176,6 @@ class Gene:
         self.ec = set()
         self.ko = set()
         
-for o in orgList:
-    overviewF = open("overview_{}.csv".format(o), "w")
-    selectedEcToGene = None
-    geneData = defaultdict(Gene)
-    selectedEcToGene = ecToGeneOrg[o]
-    for k in kegg:
-        for ec in kegg[k]:
-            for gene in selectedEcToGene[ec]:
-                geneName = gene
-                    
-                geneData[geneName].kegg.add(k)
-                geneData[geneName].ec.add(ec)
-                for ko in KOToEC:
-                    if ec in KOToEC[ko]:
-                        geneData[geneName].ko.add(ko)
-    overviewF.write("KEGG\n")
-    
-    for gene in geneData:
-        for ko in geneData[gene].ko:
-            for ec in geneData[gene].ec:
-                for k in geneData[gene].kegg:
-                    overviewF.write("{}\n".format('\t'.join([gene, ko, ec, k])))
 
         
         
--- kegg_go/go2excel.py
+++ kegg_go/go2excel.py
@@ -3,58 +3,17 @@ import sys
 import os
 
 """
-python3 go2excel.py ../renaming/MB42_renamed_genes.csv ../renaming/LEV6574_renamed_genes.csv goMB42.csv goLEV.csv others.csv
+python3 go2excel.py go.csv
 """
-renamingMB42 = {}
-renamingLEV = {}
 processed = set()
 
-# get ids MB42
-for l in open(sys.argv[1],"r"):
-    l = l.strip().split("\t")
-    renamingMB42[l[0] + "MB42"] = l[1]
-
-# get ids LEV
-for l in open(sys.argv[2],"r"):
-    l = l.strip().split("\t")
-    renamingLEV[l[0] + "LEV"] = l[1]
 
 
 currentGene = ""
 print("\t".join(["Gene", "Category", "Term", "ID"]))
 
-for l in open(sys.argv[3],"r"):
-    if "<xref id=" in l:
-        currentGene = l.split('"')[1] + "MB42"
-    elif "<go-xref" in l:
-        l = l.split('"')
-        goID = l[5].split(":")[1]
-        goCat = l[1]
-        goTerm = l[7]
-        if currentGene in renamingMB42:
-            line = "\t".join([renamingMB42[currentGene], goCat, goTerm, goID])
-            if line not in processed: # add only if valid gene after renaming
-                print(line)
-                processed.add(line)
-            
 
-for l in open(sys.argv[4],"r"):
-    if "<xref id=" in l:
-        currentGene = l.split('"')[1] + "LEV"
-    elif "<go-xref" in l:
-        l = l.split('"')
-        if len(l) >=8:
-            goID = l[5].split(":")[1]
-            goCat = l[1]
-            goTerm = l[7]
-
-            if currentGene in renamingLEV:
-                line = "\t".join([renamingLEV[currentGene], goCat, goTerm, goID])
-                if line not in processed: # add only if valid gene after renaming
-                    print(line)
-                    processed.add(line)
-
-for l in open(sys.argv[5],"r"):
+for l in open(sys.argv[2],"r"):
     if "<xref id=" in l:
         currentGene = l.split('"')[1]
     elif "<go-xref" in l and "<model" not in l:

d87c8cb6027845a2df8b1a346eb0172bb754c9f4
Sven Warris <sven@warris.nl>
2018-01-19 14:52:59 +0100

    process now per species

--- kegg_go/addCount2DB.py
+++ kegg_go/addCount2DB.py
@@ -2,23 +2,30 @@ import sys
 from collections import defaultdict
 from neo4j.v1 import GraphDatabase, basic_auth
 
-hostname = "phi-host.ab.wurnet.nl"
+hostname = "wurnfs"
 username = "neo4j"
-password = "Neo4J"
+password = "cytoscape"
 
 driver = GraphDatabase.driver("bolt://{}".format(hostname), auth=basic_auth(username, password))
 session = driver.session()
 
 currentGene = ""
 processed = set()
-for f in sys.argv[1:]:
+for f in sys.argv[2:]:
+    proteinMapping = {}
+    orgName = f.split("/")[-1].split(".")[0]
+    print(orgName)
+    for l in open(f,"r"):
+        if len(l) > 0 and l[0] == ">":
+            proteinMapping[l[1:].split(" ")[0].strip()] = orgName
+    #reset data
     session.run("match (a:GOTerm) set a.fold = 0, a.up = 0, a.down = 0, a.neutral = 0, a.allUp = 0, a.allDown=0, a.allNeutral = 0, a.averageFold = 0, a.allFold = 0")
-    for l in open(f, "r"):
+    for l in open(sys.argv[1], "r"):
         if "<xref id=" in l:
             currentGene = l.split('"')[1]
         elif "<go-xref" in l and "<model" not in l:
             l = l.split('"')
-            if len(l) >=8:
+            if len(l) >=8 and currentGene in proteinMapping:
                 goID = l[5]
                 goCat = l[1]
                 goTerm = l[7]
@@ -39,8 +46,9 @@ for f in sys.argv[1:]:
     goTerms = ['GO:0008150','GO:0005575','GO:0003674']
     for level in  ["1","2"]:
         topLine = True
-        resultFile = open(level + "_" + f, "w")
+        resultFile = open(level + "_" + orgName + ".tsv", "w")
         for go in goTerms:
+            print("Level {} on term {}".format(level, go))
             result = session.run("Match (a:GOTerm {id:'"+go+"'})<-[:ISA*"+ level +"]-(c) where c.up is not null and c.up > 0 with distinct c as n return n.up, n.name, n.id, n.namespace order by n.up desc")
             #print(result)
             for r in result:
--- kegg_go/go2excel.py
+++ kegg_go/go2excel.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 import sys
 import os
+from _mysql import result
 
 """
 python3 go2excel.py go.csv
@@ -10,19 +11,27 @@ processed = set()
 
 
 currentGene = ""
-print("\t".join(["Gene", "Category", "Term", "ID"]))
 
 
-for l in open(sys.argv[2],"r"):
-    if "<xref id=" in l:
-        currentGene = l.split('"')[1]
-    elif "<go-xref" in l and "<model" not in l:
-        l = l.split('"')
-        if len(l) >=8:
-            goID = l[5].split(":")[1]
-            goCat = l[1]
-            goTerm = l[7]
-            line = "\t".join([currentGene, goCat, goTerm, goID])
-            if line not in processed:
-                print(line)
-                processed.add(line)
+for f in sys.argv[2:]:
+    proteinMapping = {}
+    orgName = f.split("/")[-1].split(".")[0]
+    print(orgName)
+    resultFile = open("goterms_" + orgName + ".tsv", "w")
+    resultFile.write("\t".join(["Gene", "Category", "Term", "ID"]) + "\n") 
+    for l in open(f,"r"):
+        if len(l) > 0 and l[0] == ">":
+            proteinMapping[l[1:].split(" ")[0].strip()] = orgName
+    for l in open(sys.argv[1], "r"):
+        if "<xref id=" in l:
+            currentGene = l.split('"')[1]
+        elif "<go-xref" in l and "<model" not in l:
+            l = l.split('"')
+            if len(l) >=8 and currentGene in proteinMapping:
+                goID = l[5].split(":")[1]
+                goCat = l[1]
+                goTerm = l[7]
+                line = "\t".join([currentGene, goCat, goTerm, goID])
+                if line not in processed:
+                    resultFile.write(line+"\n")
+                    processed.add(line)

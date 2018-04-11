8798e72e3ebb4b66b9d456b1ae7ffa1fac31b4f3
Sven Warris <sven@warris.nl>
2018-04-11 09:29:05 +0200

    Added links between enzymes and compounds

--- kegg_go/linkKEGG2GO.py
+++ kegg_go/linkKEGG2GO.py
@@ -17,11 +17,13 @@ password = "cytoscape"
 driver = GraphDatabase.driver("bolt://{}".format(hostname), auth=basic_auth(username, password))
 session = driver.session()
 
-pattern = re.compile("[\A\s]GO: ([0-9]*)")
-
+pattern = re.compile("[\A\s]GO: ([0-9\s]+)")
+compound = re.compile("\[CPD:(C[0-9]*)\]")
 result = session.run("match(a:enzyme) return a.name as name")
 for r in result:
     print(r["name"])
+    productFound = False
+    substrateFound = False
     for ecInfo in kegg_get(r["name"]):
         ecInfoLabel = ecInfo[:12]
         if "ORTHOLOGY" in ecInfoLabel:
@@ -30,7 +32,31 @@ for r in result:
             for ko in kegg_get(ecInfo[12:18].strip()):
                 match = pattern.findall(ko) 
                 if len(match) > 0:
-                    session.run("match (a:enzyme{{name: '{name}'}}), (g:GOTerm{{id: 'GO:{go}'}}) merge (a)-[r:xreference]->(g)".format(name=r["name"], go=match[0]))
+                    match = ko.strip().split(" ")[1:]
+                    for m in match:
+                        print(m)
+                        session.run("match (a:enzyme{{name: '{name}'}}), (g:GOTerm{{id: 'GO:{go}'}}) merge (a)-[r:xreference]->(g)".format(name=r["name"], go=m.strip()))
+        if "PRODUCT" in ecInfoLabel or productFound:
+            productFound = True
+            match = compound.findall(ecInfo) 
+            if len(match) > 0:
+                for m in match:
+                    session.run("match (a:enzyme{{name: '{name}'}}), (c:compound{{name: 'cpd:{compound}'}}) merge (a)-[r:produces]->(c)".format(name=r["name"], compound=m))
+            else:
+                productFound = False
+            if ';' not in ecInfo:
+                productFound = False
+                    
+        if "SUBSTRATE" in ecInfoLabel or substrateFound:
+            substrateFound = True
+            match = compound.findall(ecInfo) 
+            if len(match) > 0:
+                for m in match:
+                    session.run("match (a:enzyme{{name: '{name}'}}), (c:compound{{name: 'cpd:{compound}'}}) merge (a)<-[r:usedby]-(c)".format(name=r["name"], compound=m))
+            else:
+                substrateFound = False
+            if ';' not in ecInfo:
+                substrateFound = False
 session.close()
 
 
